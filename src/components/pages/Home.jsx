f88157e6aaf7e4169a545c8a433a48dec113be4e
Pratham_singh <pratham@teamvariance.com>
2022-09-18 02:13:22 +0530

    Pagination working

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,8 +5,10 @@ import axios from "axios";
 import _ from "lodash";
 
 const Home = () => {
+  const pageSize = 5;
   const [users, setUser] = useState([]);
-
+  const [paginateUsers, setPaginateUsers] = useState();
+  const [currentPage, setCurrentPage] = useState();
   useEffect(() => {
     loadUsers();
   }, []);
@@ -16,6 +18,7 @@ const Home = () => {
       const response = await axios.get("http://localhost:3003/users");
       console.log(response);
       setUser(response.data.reverse()); //using reverse to view new added user on top
+      setPaginateUsers(_(response.data).slice(0).take(pageSize).value());
     } catch {
       console.log("error");
     }
@@ -31,17 +34,31 @@ const Home = () => {
 
   // Pagination
 
-  return (
-    <div className="container">
-      <div className="py-4">
-        <div className="heading">
-          <h1>Home Page</h1>
-          <Link className="add-user" exact to="/users/add">
-            ADD+
-          </Link>
-        </div>
+  const pageCount = users ? Math.ceil(users.length / pageSize) : 0;
+  if (pageCount === 1) return null;
 
-        {/* {
+  const pages = _.range(1, pageCount + 1);
+  const pagination = (pageNo) => {
+    setCurrentPage(pageNo);
+    const startIndex = (pageNo - 1) * pageSize;
+    const paginateUsers = _(users).slice(startIndex).take(pageSize).value();
+    setPaginateUsers(paginateUsers);
+  };
+  return (
+    <div>
+      {!users ? (
+        "no data found"
+      ) : (
+        <div className="container">
+          <div className="py-4">
+            <div className="heading">
+              <h1>Home Page</h1>
+              <Link className="add-user" exact to="/users/add">
+                ADD+
+              </Link>
+            </div>
+
+            {/* {
 
          -----Card format view----
 
@@ -59,50 +76,64 @@ const Home = () => {
             </div >
           ))} */}
 
-        {/* -----for Table format view---- */}
+            {/* -----for Table format view---- */}
 
-        <table className="table table-success table-striped border shadow">
-          <thead className="table-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Age</th>
-              <th scope="col">Department</th>
-              <th scope="col">Blood Grp</th>
-              <th scope="col">Address</th>
-              <th scope="col">Contact No</th>
-              <th scope="col">Actions</th>
-            </tr>
-          </thead>
-          <tbody className="table-group-divider">
-            {users.map((user, index) => (
-              <tr>
-                <th scope="row">{index + 1}</th>
-                <td>{user.name}</td>
-                <td>{user.age}</td>
-                <td>{user.department}</td>
-                <td>{user.blood}</td>
-                <td>{user.address}</td>
-                <td>{user.phone}</td>
-                <td>
-                  <Link
-                    className="btn btn-primary my-1 edit"
-                    to={`/users/edit/${user.id}`}
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => deleteUser(user.id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            <table className="table table-success table-striped border shadow">
+              <thead className="table-dark">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Age</th>
+                  <th scope="col">Department</th>
+                  <th scope="col">Blood Grp</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Contact No</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody className="table-group-divider">
+                {paginateUsers?.map((user, index) => (
+                  <tr>
+                    <th scope="row">{index + 1}</th>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                    <td>{user.department}</td>
+                    <td>{user.blood}</td>
+                    <td>{user.address}</td>
+                    <td>{user.phone}</td>
+                    <td>
+                      <Link
+                        className="btn btn-primary my-1 edit"
+                        to={`/users/edit/${user.id}`}
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => deleteUser(user.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <nav aria-label="...">
+              <ul class="pagination pagination-md">
+                {pages.map((page) => (
+                  <li class="page-item ">
+                    < button class="page-link" onClick={() => pagination(page)}>
+                      {page}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
